e045bdb50936c06845d97de92beff4d905199c08
SiliconeiOS <johnsmithjunior28122000@gmail.com>
2025-08-30 16:00:12 +0300

    Initial Commit

---
+++ WhatTheForecast/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WhatTheForecast
+//
+//  Created by Иван Дроботов on 8/30/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ WhatTheForecastTests/WhatTheForecastTests.swift
@@ -0,0 +1,17 @@
+//
+//  WhatTheForecastTests.swift
+//  WhatTheForecastTests
+//
+//  Created by Иван Дроботов on 8/30/25.
+//
+
+import Testing
+@testable import WhatTheForecast
+
+struct WhatTheForecastTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
